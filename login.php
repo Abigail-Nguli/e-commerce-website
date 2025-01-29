d87fa359a9ea252ea658e41950235559631c104c
Abigail-Nguli <143516715+Abigail-Nguli@users.noreply.github.com>
2025-01-29 15:38:56 +0300

    Update file names

--- login.php
+++ login.php
@@ -12,6 +12,16 @@
             <form action="#" class="login-form" method="POST">
                 <h1 class="login-title">Login</h1>
 
+                <div id="message">
+                    <?php 
+                        if(isset($_SESSION['login']))
+                        {
+                            echo $_SESSION['login'];
+                            unset($_SESSION['login']);
+                        }
+                    ?>
+                </div>
+
                 <div class="input-box">
                     <i class="fa-regular fa-user"></i>
                     <input type="text" name="username" placeholder="Username">
@@ -29,16 +39,51 @@
                     </label>
                     <a href="#">Forgot Password?</a>
                 </div>
-
                 <input type="submit" name="submit" value="Login" class="login-btn">
 
                 <p class="register">
                     Don't have an account?
-                    <a href="register.html">Register</a>
+                    <a href="register.php">Register</a>
                 </p>
             </form>
 
         </div>
+    </body>
+</html>
+
+        <?php 
+    //Check Whether Login button is clicked
+    if(isset($_POST['submit']))
+    {
+        //Process for Login
+        //1.Get Data from Login Form
+        $username = $_POST['username'];
+        $password = md5($_POST['password']);
+
+        //Query to check whether the user with usernam and password exist or not
+        $sql = "SELECT * FROM tbl_user WHERE username = '$username' AND password = '$password'";
+
+        //Execute Query
+        $res = mysqli_query($conn, $sql);
+
+        //Count rows to check whether user exists or not
+        $count = mysqli_num_rows($res);
+
+        if($count==1)
+        {
+            //User Available and Login Success
+            $_SESSION['login'] = "<div class='success text-center'>Login Successful</div>";
+            $_SESSION['user'] = $username; //Checks whether user is logged in or not and logout will unset it
+            header("location:"'cart.html');
+        }
+        else
+        {
+            //User Not Availabe and Login Failed
+            $_SESSION['login'] = "<div class='error text-center'>Username or Password did not match</div>";
+            header("location:"'login.php');
+        }
+    }
+?>
 
         <script> 
             // JavaScript to hide message after 5 seconds 
@@ -49,6 +94,3 @@
                 }, 5000); 
             // 5000 milliseconds = 5 seconds
         </script>
-
-    </body>
-</html>
--- register.php
+++ register.php
@@ -12,6 +12,16 @@
             <form action="#" class="login-form" method="POST">
                 <h1 class="login-title">Register</h1>
 
+                <div id="message">
+                    <?php 
+                        if(isset($_SESSION['register']))
+                        {
+                            echo $_SESSION['register'];
+                            unset($_SESSION['register']);
+                        }
+                    ?>
+                </div>
+
                 <div class="input-box">
                     <i class="fa-regular fa-user"></i>
                     <input type="text" name="full_name" placeholder="Full Name" required>
@@ -32,6 +42,40 @@
             </form>
 
         </div>
+    </body>
+</html>
+
+        <?php 
+   if(isset($_POST['submit'])){
+
+        //Get Data
+        $full_name = $_POST['full_name'];
+        $username = $_POST['username'];
+        $password = md5($_POST['password']);
+
+
+        //Save Data
+        $sql = "INSERT INTO tbl_user SET
+            full_name = '$full_name',
+            username = '$username',
+            password = '$password'
+        ";
+
+
+        $res = mysqli_query($conn, $sql) or die(mysqli_error());
+        if($res==TRUE){
+            $_SESSION['register'] = "<div class='success'>Registration Successfully</div>";
+
+            header("location:"'login.php');
+        }
+        else{
+            $SESSION['register'] = "<div class='error'>❌Failed to Register. Try Again Later!!</div>";
+
+            header("location:"'register.php');
+        }
+    }
+?>
+
 
         <script> 
             // JavaScript to hide message after 5 seconds 
@@ -43,8 +87,5 @@
             }, 5000); 
             // 5000 milliseconds = 5 seconds
         </script>
-
-    </body>
-</html>
 
 
